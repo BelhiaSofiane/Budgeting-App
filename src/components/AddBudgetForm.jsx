e4127e5cdbe04d3b0d939545811d9f3778568a7d
Belhia Sofiane Mekki <sofianebelhia150@gmail.com>
2025-06-23 02:19:36 +0100

    Added new expenses form

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,22 +1,38 @@
+// rrd imports
+import { Form, useFetcher } from 'react-router-dom'
+// react imports
+import { useEffect, useRef } from 'react';
+// library
 import { CurrencyDollarIcon } from '@heroicons/react/24/outline'
-import React from 'react'
-import { Form } from 'react-router-dom'
 
 const AddBudgetForm = () => {
-  return (
+    const fetcher = useFetcher();
+    const isSubmitting = fetcher.state === 'submitting';
+
+    const formRef = useRef();
+    const focusRef = useRef();
+
+    useEffect(() => {
+        if (!isSubmitting) {
+            formRef.current.reset();
+            focusRef.current.focus();
+        }
+    },[isSubmitting])
+    return (
     <div className='form-wrapper'>
         <h2 className='h3'>
-            create budget
+            Create budget
         </h2>
-        <Form className='grid-sm' method='post'>
+        <fetcher.Form className='grid-sm' method='post' ref={formRef}>
             <div className="grid-xs">
                 <label htmlFor="newBudget">Budget Name</label>
                 <input 
                 type="text" 
                 name='newBudget' 
                 id='newBudget'
-                required
                 placeholder='e.g. Groceries, Rent, etc.'
+                required
+                ref={focusRef}
                 />
             </div>
             <div className="grid-xs">
@@ -31,11 +47,22 @@ const AddBudgetForm = () => {
                 inputMode='decimal'
                 />
             </div>
-            <button className='btn btn--dark' type='submit'>
-                <span>Create Budget</span>
-                <CurrencyDollarIcon className='icon' width={20} />
+            <input type="hidden" name="_action" value="createBudget" />
+            <button 
+            className='btn btn--dark' 
+            type='submit'
+            disabled={isSubmitting}
+            >
+                {
+                    isSubmitting ? <span>Creating Budget...</span> : (
+                        <>
+                            <span>Create Budget</span>
+                            <CurrencyDollarIcon className='icon' width={20} />
+                        </>
+                    )
+                }
             </button>        
-        </Form>
+        </fetcher.Form>
     </div>
   )
 }
--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,44 @@
+export const waait = () => new Promise(resolve => setTimeout(resolve, Math.random() * 800));
+
+const generateRandomColor = () => {
+    const existingBudgetsLength = fetchData("budgets")?.length ?? 0;
+    return `${existingBudgetsLength * 34 } 65% 50%`
+}
+
 // Local Storage 
 export const fetchData = (key) => {
     return JSON.parse(localStorage.getItem(key));
+}
+
+// create budget 
+export const createBudget = ({ name, amount }) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        color: generateRandomColor(),
+    }
+    const existingBudgets = fetchData("budgets") ?? [];
+    return localStorage.setItem("budgets", JSON.stringify([...existingBudgets, newItem]));
+}
+// create Expense 
+export const createExpense = ({ 
+    name, amount, budgetId 
+}) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        budgetId: budgetId, // This should be set based on the selected budget
+    }
+    const existingExpenses = fetchData("expenses") ?? [];
+    return localStorage.setItem("expenses", JSON.stringify([...existingExpenses, newItem]));
 }
 
 // delete item
 export const deleteItem = ({ key }) => {
     return localStorage.removeItem(key);
 }
+
